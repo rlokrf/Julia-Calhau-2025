7162a5dd2ccfab7be2d98e7276b96d5f1d435fdc
PedroDuarte2007 <160654602+PedroDuarte2007@users.noreply.github.com>
2024-02-27 17:10:59 -0300

    Add files via upload

---
+++ soma.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	int a,b,c;
+	printf("digite um numeros inteiros");
+	scanf("%d",&a);
+	
+	printf("digite um numeros inteiros");
+	scanf("%d",&b);
+	c = (a + b);
+	
+	printf("%d",c);
+}
